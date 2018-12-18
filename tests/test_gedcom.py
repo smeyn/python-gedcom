46c8302c42fc01d736f03aef62f4741013fb5077
Nicklas Reincke <contact@reynke.com>
2018-12-18 20:13:24 +0100

    Added first test and test data

--- tests/test_gedcom.py
+++ tests/test_gedcom.py
@@ -1,2 +1,16 @@
-def test_success():
-    assert True
+import unittest
+
+from os import path
+from gedcom.gedcom import Gedcom
+
+here = path.abspath(path.dirname(__file__))
+test_files = path.join(here, 'test_files/')
+
+
+class TestGedcom(unittest.TestCase):
+
+    def setUp(self):
+        self.gedcom = Gedcom(path.join(test_files, 'Mustermann.ged'))
+
+    def test_parses_successfully(self):
+        assert len(self.gedcom.get_element_list()) == 376
